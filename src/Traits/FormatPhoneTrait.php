4c36293dff6621f30d343141f4f5165ad39c27d8
Pranasucitra <lingga.pranasucitra@gmail.com>
2025-01-07 18:31:10 +0700

    Initial commit

---
+++ src/Traits/FormatPhoneTrait.php
@@ -0,0 +1,69 @@
+<?php
+/*
+* File: FormatPhoneTrait.php
+* Project: Traits
+* Created Date: Mo Oct 2022
+* Author: Ayatulloh Ahad R
+* Email: ayatulloh@indiega.net
+* Phone: 085791555506
+* -------------------------
+* Last Modified: Mon Oct 03 2022
+ * Modified By: Ayatulloh Ahad R
+ * -------------------------
+ * Copyright (c) 2022 Indiega Network 
+ 
+ * -------------------------
+ * HISTORY:
+ * Date      	By	Comments 
+ 
+ * ----------	---	---------------------------------------------------------
+ * update:
+ * 1. menambahkan locale untuk kode negara. Default ID
+ */
+
+namespace pranasucitra\WhatsAppHelper\Traits;
+
+use Exception;
+
+trait FormatPhoneTrait
+{
+
+    /**
+     * @var string
+     * @author Ayatulloh Ahad R <ayatulloh@indiega.net>
+     */
+    protected $locale   = 'ID';
+
+    /**
+     * Helper berfungsi untuk memformat nomor telepon 
+     *
+     * @param  int|null $phone
+     *
+     * @return void            
+     * @author Ayatulloh Ahad R <ayatulloh@indiega.net>
+     * @throws Exception
+     */
+    public function FormatPhone(int $phone = null)
+    {
+        $phoneUtil              = \libphonenumber\PhoneNumberUtil::getInstance();
+        try {
+
+            $NumberFormat       = $phoneUtil->parse($phone, $this->locale);
+            return $NumberFormat->getCountryCode() . $NumberFormat->getNationalNumber();
+            var_dump($NumberFormat);
+        } catch (\libphonenumber\NumberParseException $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+	/**
+	 * Set the value of locale
+	 * @param   string  $locale  
+	 * @return  self
+	 */
+	public function setLocale(string $locale)
+	{
+		$this->locale = $locale;
+		return $this;
+	}
+}
---
+++ test.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * File: test.php
+ * Project: WhatsAppHelper
+ * Created Date: Mo Oct 2022
+ * Author: Ayatulloh Ahad R
+ * Email: ayatulloh@indiega.net
+ * Phone: 085791555506
+ * -------------------------
+ * Last Modified: Mon Oct 03 2022
+ * Modified By: Ayatulloh Ahad R
+ * -------------------------
+ * Copyright (c) 2022 Indiega Network 
+
+ * -------------------------
+ * HISTORY:
+ * Date      	By	Comments 
+
+ * ----------	---	---------------------------------------------------------
+ */
+
+use pranasucitra\WhatsAppHelper\WhatsAppSG;
+
+require 'vendor/autoload.php';
+
+/* ------------------------ contoh penggunaan pertama ----------------------- */
+
+$wa     = new WhatsAppSG();
+$wa->setPort('6789')
+    ->setSenderPhone('085791555506')
+    ->setRecepient('082255879157')
+    ->setMessage('test kirim wa?');
+
+var_dump($wa->SendText());
+
+/**
+ * jika anda menggunakan nomor diluar indonesia 
+ * Anda dapat mengatur locale dengan cara
+ * 
+ * catatan:
+ * lihat kode negara aplha-2-code di link berikut
+ * https://www.iban.com/country-codes
+ */
+$wa     = new WhatsAppSG();
+$wa->setPort('6789')
+    ->setLocale('US')
+    ->setSenderPhone('085791555506')
+    ->setRecepient('082255879157')
+    ->setMessage('test kirim wa?');
+
+var_dump($wa->SendText());
+
+/* ------------------------- contoh penggunaan kedua ------------------------ */
+
+$wa     = new WhatsAppSG('082255879157', 'hallo ini coba kirim whatsapp');
+$wa->setBaseUrl('http://127.0.0.1')
+    ->setPort('6789')
+    ->setSenderPhone('085791555506');
+
+$result     = $wa->SendText();
